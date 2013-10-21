e82b3e8dd0fe2f5d9699861c4682c480fc3ce4d4
Alex Bleasdale <ableasdale@marklogic.com>
2013-10-21 15:50:24 +0100

    Replaced Log4j with LogBack and the simplified XccLoadBalancer example

---
+++ src/main/java/com/xmlmachines/XccLoadBalancerExample.java
@@ -0,0 +1,11 @@
+package com.xmlmachines;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 21/10/13
+ * Time: 15:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class XccLoadBalancerExample {
+}
